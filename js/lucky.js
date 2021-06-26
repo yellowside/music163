378c5f0b189f05f2fa6696145495aeee83af2be0
yellowside <30918006+yellowside@users.noreply.github.com>
2021-06-26 15:20:13 +0800

    Update lucky.js

--- js/lucky.js
+++ js/lucky.js
@@ -2,7 +2,7 @@ wy_title = $('title').text()
 function loading() {
     $.ajax({
         type: 'GET',
-        url: 'https://yuan.cuan.la/music.php',
+        url: 'https://api.bingdou.xyz/music/music.php',
 
         data: 'format=json&id=3778678',// 可以在这里指定歌单,也可以不指定
         dataType: 'json',
